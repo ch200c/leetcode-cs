198050c6b0272466c70361549470bdc7eb552257
ch200c <an.dris.developer@gmail.com>
2020-04-01 21:07:36 +0100

    p136

---
+++ LeetCode/P136/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.P136
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var duplicates = new HashSet<int>();
+
+            foreach (var num in nums)
+            {
+                if (duplicates.Contains(num))
+                {
+                    duplicates.Remove(num);
+                }
+                else
+                {
+                    duplicates.Add(num);
+                }
+            }
+
+            return duplicates.First();
+        }
+    }
+}
