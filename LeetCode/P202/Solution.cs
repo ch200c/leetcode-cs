55f4590ed9ed451dc349e8364538ef3fa961c14b
ch200c <an.dris.developer@gmail.com>
2020-04-02 20:06:01 +0100

    p202

---
+++ LeetCode/P202/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.P202
+{
+    public class Solution
+    {
+        public bool IsHappy(int n)
+        {
+            var history = new HashSet<int>();
+
+            while (!history.Contains(n))
+            {
+                history.Add(n);
+                var digits = ToDigits(n);
+                n = digits.Sum((digit) => (int)Math.Pow(digit, 2));
+            }
+
+            return n == 1;
+        }
+
+        private List<int> ToDigits(int n)
+        {
+            var digits = new List<int>();
+
+            while (n >= 10)
+            {
+                digits.Add(n % 10);
+                n /= 10;
+            }
+
+            digits.Add(n);
+            digits.Reverse();
+
+            return digits;
+        }
+    }
+}
---
+++ UnitTests/P202/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.P202
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new LeetCode.P202.Solution();
+            var result = solution.IsHappy(19);
+            Assert.True(result);
+        }
+    }
+}
