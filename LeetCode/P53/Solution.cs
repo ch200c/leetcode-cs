ea9cd6cde4ee101ee01b3432c5e1c9991e62ea9a
ch200c <an.dris.developer@gmail.com>
2020-04-03 17:44:05 +0100

    p53

---
+++ LeetCode/P53/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.P53
+{
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            var largestSum = int.MinValue;
+
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                var sum = nums[i];
+
+                if (sum > largestSum)
+                {
+                    largestSum = sum;
+                }
+
+                for (var j = i + 1; j < nums.Length; ++j)
+                {
+                    sum += nums[j];
+
+                    if (sum > largestSum)
+                    {
+                        largestSum = sum;
+                    }
+                }
+            }
+
+            return largestSum;
+        }
+    }
+}
---
+++ UnitTests/P53/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.P53
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new LeetCode.P53.Solution();
+            var result = solution.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
+            Assert.Equal(6, result);
+        }
+    }
+}
