1ea906e2aaef53975821683cce3f30fc0538d957
ch200c <an.dris.developer@gmail.com>
2020-04-06 19:41:59 +0100

    p49

---
+++ LeetCode/P49/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.P49
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var sortedStringToAnagrams = new Dictionary<string, List<string>>();
+
+            for (var i = 0; i < strs.Length; ++i)
+            {
+                if (sortedStringToAnagrams.ContainsKey(strs[i]))
+                {
+                    sortedStringToAnagrams[strs[i]].Add(strs[i]);
+                }
+                else
+                {
+                    var sortedString = new string(strs[i].OrderBy(c => c).ToArray());
+                    if (sortedStringToAnagrams.ContainsKey(sortedString))
+                    {
+                        sortedStringToAnagrams[sortedString].Add(strs[i]);
+                    }
+                    else
+                    {
+                        sortedStringToAnagrams[sortedString] = new List<string>() { strs[i] };
+                    }
+                }
+            }
+
+            var groups = new List<IList<string>>();
+
+            foreach (var key in sortedStringToAnagrams.Keys)
+            {
+                groups.Add(sortedStringToAnagrams[key]);
+            }
+
+            return groups;
+        }
+    }
+}
---
+++ UnitTests/P49/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+namespace UnitTests.P49
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new LeetCode.P49.Solution();
+            var result = solution.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+
+            var expectedResult = new List<IList<string>>() {
+                new List<string>() { "ate", "eat", "tea" },
+                new List<string>() { "nat", "tan" },
+                new List<string>() { "bat" } };
+
+            // TODO
+            Assert.Subset(expectedResult[0].ToHashSet(), result[0].ToHashSet());
+            Assert.Subset(expectedResult[1].ToHashSet(), result[1].ToHashSet());
+            Assert.Subset(expectedResult[2].ToHashSet(), result[2].ToHashSet());
+        }
+    }
+}
