a7f536e42de3d3808d39656b3748caf30cfff626
ch200c <an.dris.developer@gmail.com>
2020-04-09 20:49:02 +0100

    P844

---
+++ LeetCode/P844/A1/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.P844.A1
+{
+    public class Solution
+    {
+        public bool BackspaceCompare(string S, string T)
+        {
+            var sStack = new Stack<char>();
+            for (var i = 0; i < S.Length; ++i)
+            {
+                if (S[i]=='#')
+                {
+                    sStack.TryPop(out _);
+                }
+                else
+                {
+                    sStack.Push(S[i]);
+                }
+            }
+
+            var tStack = new Stack<char>();
+            for (var i = 0; i < T.Length; ++i)
+            {
+                if (T[i] == '#')
+                {
+                    tStack.TryPop(out _);
+                }
+                else
+                {
+                    tStack.Push(T[i]);
+                }
+            }
+
+            return sStack.SequenceEqual(tStack);
+        }
+    }
+}
---
+++ LeetCode/P844/A2/Solution.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.P844.A2
+{
+    public class Solution
+    {
+        private LinkedListNode<char> ToLinkedList(string input)
+        {
+            var backspaceCount = 0;
+            LinkedListNode<char> currentNode = null;
+            var chars = new LinkedList<char>();
+
+            for (var i = input.Length - 1; i >= 0; --i)
+            {
+                if (input[i] == '#')
+                {
+                    ++backspaceCount;
+                    continue;
+                }
+                else
+                {
+                    if (backspaceCount > 0)
+                    {
+                        --backspaceCount;
+                        continue;
+                    }
+                    else
+                    {
+                        if (currentNode != null)
+                        {
+                            currentNode = chars.AddAfter(currentNode, input[i]);
+                        }
+                        else
+                        {
+                            currentNode = chars.AddFirst(input[i]);
+                        }
+                    }
+                }
+            }
+
+            return currentNode;
+        }
+
+
+        public bool BackspaceCompare(string S, string T)
+        {
+            var currentSNode = ToLinkedList(S);
+            var currentTNode = ToLinkedList(T);
+
+            if (currentSNode == null && currentTNode == null)
+            {
+                return true;
+            }
+
+            if ((currentSNode == null && currentTNode != null) ||
+                (currentSNode != null && currentTNode == null))
+            {
+                return false;
+            }
+
+            if (currentSNode.List.Count != currentTNode.List.Count)
+            {
+                return false;
+            }
+
+            while (currentSNode != null && currentTNode != null)
+            {
+                if (currentSNode.Value != currentTNode.Value)
+                {
+                    return false;
+                }
+
+                currentSNode = currentSNode.Previous;
+                currentTNode = currentTNode.Previous;
+            }
+
+            return true;
+        }
+    }
+}
