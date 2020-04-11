b43f666316aeeb632ab90fcb1eb596851bf521d3
ch200c <an.dris.developer@gmail.com>
2020-04-11 15:43:05 +0100

    p543

---
+++ LeetCode/P543/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.P543
+{
+    public class Solution
+    {
+        private int maxDiameter;
+ 
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            maxDiameter = 0;
+            Traverse(root);
+            return maxDiameter;
+        }
+
+        private int Traverse(TreeNode node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            var leftValue = Traverse(node.left);
+            var rightValue = Traverse(node.right);
+
+            maxDiameter = Math.Max(maxDiameter, leftValue + rightValue);
+            return Math.Max(leftValue, rightValue) + 1;
+        }
+    }
+}
