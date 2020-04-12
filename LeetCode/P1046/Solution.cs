56110a6611159bb41bb84ef94f7a62bc8baa33d8
ch200c <an.dris.developer@gmail.com>
2020-04-12 12:31:50 +0100

    p1046

---
+++ LeetCode/P1046/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.P1046
+{
+    public class Solution
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            var result = -1;
+            while (result == -1)
+            {
+                Array.Sort(stones);
+
+                if (stones.Length == 1)
+                {
+                    result = stones[0];
+                    break;
+                }
+
+                var stone1 = stones[^1];
+                var stone2 = stones[^2];
+
+                if (stone1 == 0)
+                {
+                    result = stone2;
+                    break;
+                }
+
+                if (stone2 == 0)
+                {
+                    result = stone1;
+                    break;
+                }
+
+                stones[^1] = 0;
+                stones[^2] = 0;
+
+                if (stone1 != stone2)
+                {
+                    stones[^2] = Math.Max(stone1, stone2) - Math.Min(stone1, stone2);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTests/P1046/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.P1046
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new LeetCode.P1046.Solution();
+            var result = solution.LastStoneWeight(new int[] { 2, 7, 4, 1, 8, 1 });
+            Assert.Equal(1, result);
+        }
+    }
+}
