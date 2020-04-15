4106c63b787ba7a01a76b7f29c3b7fca578bfc59
ch200c <an.dris.developer@gmail.com>
2020-04-15 17:32:51 +0100

    P238

---
+++ LeetCode/P238/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.P238
+{
+    public class Solution
+    {
+        // Runtime: 264 ms   (53.77 %)
+        // Memory Usage: 35.6 MB
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var output = new int[nums.Length];
+            output[0] = 1;  // Accumulate left product
+
+            for (var i = 1; i < nums.Length; ++i)
+            {
+                output[i] = output[i - 1] * nums[i - 1];
+            }
+
+            var rightProduct = 1;
+            for (var i = nums.Length - 1; i >= 0; --i)
+            {
+                output[i] *= rightProduct;
+                rightProduct *= nums[i];
+            }
+
+            return output;
+        }
+    }
+}
