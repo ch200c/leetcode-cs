4d7f999406cf9a9c6528d0a99fd1cd1e1c5439e7
ch200c <an.dris.developer@gmail.com>
2020-04-16 21:15:06 +0100

    p678

---
+++ LeetCode/P678/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.P678
+{
+    public class Solution
+    {
+        // Runtime: 76 ms (64.86 %)
+        // Memory Usage: 21.8 MB
+        public bool CheckValidString(string s)
+        {
+            var minOpenCount = 0;
+            var maxOpenCount = 0;
+
+            foreach (var c in s)
+            {
+                minOpenCount += c == '(' ? 1 : -1;
+                maxOpenCount += c != ')' ? 1 : -1;
+
+                if (maxOpenCount < 0)
+                {
+                    break;
+                }
+
+                minOpenCount = Math.Max(minOpenCount, 0);
+            }
+
+            return minOpenCount == 0;
+        }
+    }
+}
---
+++ UnitTests/P678/Test.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace UnitTests.P678
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new LeetCode.P678.Solution();
+            var result = solution.CheckValidString("()");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new LeetCode.P678.Solution();
+            var result = solution.CheckValidString("(*)");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new LeetCode.P678.Solution();
+            var result = solution.CheckValidString("(*))");
+            Assert.True(result);
+        }
+        [Fact]
+        public void Test4()
+        {
+            var solution = new LeetCode.P678.Solution();
+            var result = solution.CheckValidString("(())((())()()(*)(*()(())())())()()((()())((()))(*");
+            Assert.False(result);
+        }
+    }
+}
