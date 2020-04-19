07c18da9cee1089124df487aa9891201271c40d3
ch200c <an.dris.developer@gmail.com>
2020-04-19 21:15:11 +0100

    p33

---
+++ LeetCode/P33/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.P33
+{
+    public class Solution
+    {
+        // Runtime: 96 ms (46.09 %)
+        // Memory Usage: 24.8 MB
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length == 0)
+            {
+                return -1;
+            }
+
+            int startIndex = 0;
+            int endIndex = nums.Length - 1;
+
+            while (startIndex <= endIndex)
+            {
+                int middleIndex = startIndex + (endIndex - startIndex) / 2;
+
+                if (nums[middleIndex] == target)
+                {
+                    return middleIndex;
+                }
+                    
+                if (middleIndex - 1 >= startIndex && nums[startIndex] <= nums[middleIndex - 1])
+                {
+                    if (nums[startIndex] <= target && target <= nums[middleIndex - 1])
+                    {
+                        endIndex = middleIndex - 1;
+                    }
+                    else
+                    {
+                        startIndex = middleIndex + 1;
+                    }  
+                }
+                else if (middleIndex + 1 <= endIndex && nums[middleIndex + 1] <= nums[endIndex])
+                {
+                    if (nums[middleIndex + 1] <= target && target <= nums[endIndex])
+                    {
+                        startIndex = middleIndex + 1;
+                    }
+                    else
+                    {
+                        endIndex = middleIndex - 1;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ UnitTests/P33/Test.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace UnitTests.P33
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new LeetCode.P33.Solution();
+            var result = solution.Search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0);
+            Assert.Equal(4,result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new LeetCode.P33.Solution();
+            var result = solution.Search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3);
+            Assert.Equal( -1,result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new LeetCode.P33.Solution();
+            var result = solution.Search(new int[] { 1 }, 1);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var solution = new LeetCode.P33.Solution();
+            var result = solution.Search(new int[] { 1,3 }, 1);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            var solution = new LeetCode.P33.Solution();
+            var result = solution.Search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 5);
+            Assert.Equal(1, result);
+        }
+    }
+}
