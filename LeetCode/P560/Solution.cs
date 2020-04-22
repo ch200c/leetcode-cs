279edba911cdb9b6e87192983b0d19d325da2d29
ch200c <an.dris.developer@gmail.com>
2020-04-22 12:42:28 +0100

    p560

---
+++ LeetCode/P560/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.P560
+{
+    public class Solution
+    {
+        // Runtime: 692 ms (19.73 %)
+        // Memory Usage: 27.1 MB
+        public int SubarraySum(int[] nums, int k)
+        {
+            var count = 0;
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                var sum = 0;
+                for (var j = i; j < nums.Length; ++j)
+                {
+                    sum += nums[j];
+                    if (sum == k)
+                    {
+                        ++count;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
