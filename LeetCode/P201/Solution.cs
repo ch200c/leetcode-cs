440c91c1431d3585e0f873c7e9691a78c7ed61b1
ch200c <an.dris.developer@gmail.com>
2020-04-23 20:20:31 +0100

    p201

---
+++ LeetCode/P201/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.P201
+{
+    public class Solution
+    {
+		// Runtime: 56 ms (65.91 %)
+		// Memory Usage: 15.6 MB
+		public int RangeBitwiseAnd(int m, int n)
+        {
+			var count = 0;
+			while (m != n)
+			{
+				m >>= 1;
+				n >>= 1;
+				++count;
+			}
+
+			return n << count;
+		}
+    }
+}
