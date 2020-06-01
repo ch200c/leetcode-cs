49775b6ce77b00091d3aeaece8c75303e7e1bd3f
ch200c <an.dris.developer@gmail.com>
2020-06-01 16:45:32 +0100

    p226

---
+++ LeetCode/P226/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.P226
+{
+    public class Solution
+    {
+        private void Invert(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            var leftNode = root.left;
+            root.left = root.right;
+            root.right = leftNode;
+            Invert(root.left);
+            Invert(root.right);
+        }
+
+        // Runtime: 92 ms (78.91 %)
+        // Memory Usage: 24.2 MB
+        public TreeNode InvertTree(TreeNode root)
+        {
+            Invert(root);
+            return root;
+        }
+    }
+}
