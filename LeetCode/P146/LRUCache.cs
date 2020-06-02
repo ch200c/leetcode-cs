a9dcbfe6ae1b284f007c4fd8e9c97ae4a6edfbc8
ch200c <an.dris.developer@gmail.com>
2020-06-02 19:43:55 +0100

    p146

---
+++ LeetCode/P146/LRUCache.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.P146
+{
+    // Runtime: 404 ms (21.83 %)
+    // Memory Usage: 50.3 MB
+    public class LRUCache
+    {
+        readonly private int capacity;
+        readonly private Dictionary<int, int> cache;
+        readonly private LinkedList<int> history;
+
+        public LRUCache(int capacity)
+        {
+            this.capacity = capacity;
+            cache = new Dictionary<int, int>();
+            history = new LinkedList<int>();
+        }
+
+        public int Get(int key)
+        {
+            if (cache.ContainsKey(key))
+            {
+                history.Remove(key);
+                history.AddLast(key);
+                return cache[key];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void Put(int key, int value)
+        {
+            if (cache.Count >= capacity && !cache.ContainsKey(key))
+            {
+                var oldestValue = history.First.Value;
+                cache.Remove(oldestValue);
+                history.Remove(oldestValue);
+            }
+
+            if (cache.ContainsKey(key))
+            {
+                history.Remove(key);
+            }
+
+            history.AddLast(key);
+            cache[key] = value;
+        }
+    }
+}
---
+++ UnitTests/P146/Test.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+
+namespace UnitTests.P146
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var cache = new LeetCode.P146.LRUCache(2);
+
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            Assert.Equal(1, cache.Get(1));
+            cache.Put(3, 3);
+            Assert.Equal(-1, cache.Get(2));
+            cache.Put(4, 4);
+            Assert.Equal(-1, cache.Get(1));
+            Assert.Equal(3, cache.Get(3));
+            Assert.Equal(4, cache.Get(4));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var cache = new LeetCode.P146.LRUCache(2);
+
+            cache.Put(2, 1);
+            cache.Put(2, 2);
+            Assert.Equal(2, cache.Get(2));
+            cache.Put(1, 1);
+            cache.Put(4, 1);
+            Assert.Equal(-1, cache.Get(2));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var cache = new LeetCode.P146.LRUCache(2);
+
+            Assert.Equal(-1, cache.Get(2));
+            cache.Put(2, 6);
+            Assert.Equal(-1, cache.Get(1));
+            cache.Put(1, 5);
+            cache.Put(1, 2);
+            Assert.Equal(2, cache.Get(1));
+            Assert.Equal(6, cache.Get(2));
+        }
+    }
+}
