8a7b519a2187612745b86d570e852d5b9c2c904b
ch200c <an.dris.developer@gmail.com>
2020-06-04 17:52:19 +0100

    p344, p55

---
+++ LeetCode/P344/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.P344
+{
+    public class Solution
+    {
+        // Runtime: 364 ms (98.28%)
+        // Memory Usage: 34.7 MB
+        public void ReverseString(char[] s)
+        {
+            for (var i = 0; i < s.Length / 2; i++)
+            {
+                var previousChar = s[i];
+                s[i] = s[^(i + 1)];
+                s[^(i + 1)] = previousChar;
+            }
+        }
+    }
+}
---
+++ LeetCode/P55/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.P55
+{
+    public class Solution
+    {
+        // Runtime: 92 ms (96.32%)
+        // Memory Usage: 26.1 MB
+        public bool CanJump(int[] nums)
+        {
+            var lastIndex = nums.Length - 1;
+            for (var i = lastIndex; i >= 0; i--)
+            {
+                if (nums[i] + i >= lastIndex)
+                {
+                    lastIndex = i;
+                }
+            }
+
+            return lastIndex == 0;
+        }
+    }
+}
