197bfc5c7701f3ae5cfffeea1f5bfe5491dff511
ch200c <mantas.m@hotmail.com>
2021-09-21 16:56:42 +0300

    Upgrade to net6.0; Solve P1

---
+++ LeetCode/P1/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.P1
+{
+    public class Solution
+    {
+        // Runtime: 328 ms, faster than 49.45% of C# online submissions for Two Sum.
+        // Memory Usage: 32.7 MB, less than 41.08% of C# online submissions for Two Sum.
+#pragma warning disable CA1822 // Mark members as static
+        public int[] TwoSum(int[] nums, int target)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            throw new InvalidOperationException("Could not find indices for target");
+        }
+    }
+}
