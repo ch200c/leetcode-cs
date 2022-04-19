c065288a3e28fd77fb3927c4c66cd70d0bf14485
ch200c <mantas.m@hotmail.com>
2022-04-19 21:43:37 +0300

    P6

---
+++ LeetCode/P6/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.P6;
+
+public class Solution
+{
+    private const int HorizontalPadding = 2;
+
+    // Runtime: 136 ms, (38.25%)
+    // Memory Usage: 37.8 MB, (86.98%)
+    public string Convert(string s, int numRows)
+    {
+        var horizontalLines = new List<char>[numRows];
+
+        for (var i = 0; i < numRows; i++)
+        {
+            horizontalLines[i] = new List<char>();
+        }
+
+        var horizontalIndex = 0;
+        var isGoingUp = false;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            horizontalLines[horizontalIndex].Add(s[i]);
+
+            if (isGoingUp && horizontalIndex <= 0)
+            {
+                isGoingUp = false;
+            }
+
+            horizontalIndex = isGoingUp
+                ? horizontalIndex - 1
+                : horizontalIndex + 1;
+
+            if (horizontalIndex != 0 && horizontalIndex % numRows == 0)
+            {
+                isGoingUp = true;
+
+                horizontalIndex = numRows == 1
+                    ? 0
+                    : horizontalIndex - HorizontalPadding;
+            }
+        }
+
+        var stringBuilder = new StringBuilder();
+
+        foreach (var horizontalLine in horizontalLines)
+        {
+            stringBuilder.Append(horizontalLine.ToArray());
+        }
+
+        return stringBuilder.ToString();
+    }
+}
--- UnitTests/P4/Test.cs
+++ UnitTests/P4/Test.cs
@@ -1,29 +1,28 @@
 ﻿using Xunit;
 
-namespace UnitTests.P4
+namespace UnitTests.P4;
+
+public class Test
 {
-    public class Test
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
-        {
-            var solution = new LeetCode.P4.Solution();
-            var result = solution.FindMedianSortedArrays(
-                new int[]{ 1, 3},
-                new int[]{ 2 });
+        var solution = new LeetCode.P4.Solution();
+        var result = solution.FindMedianSortedArrays(
+            new int[] { 1, 3 },
+            new int[] { 2 });
 
-            Assert.Equal(2, result);
-        }
+        Assert.Equal(2, result);
+    }
 
-        [Fact]
-        public void Test2()
-        {
-            var solution = new LeetCode.P4.Solution();
-            var result = solution.FindMedianSortedArrays(
-                new int[] { 1, 2 },
-                new int[] { 3, 4 });
+    [Fact]
+    public void Test2()
+    {
+        var solution = new LeetCode.P4.Solution();
+        var result = solution.FindMedianSortedArrays(
+            new int[] { 1, 2 },
+            new int[] { 3, 4 });
 
-            Assert.Equal(2.5, result);
-        }
+        Assert.Equal(2.5, result);
     }
 }
