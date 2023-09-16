39ae1a4cf8ed52684f8f93393f5aaab33c72650e
ch200c <mantas.m@hotmail.com>
2023-09-16 10:55:30 +0100

    Solve P242

---
+++ LeetCode.UnitTests/P242/Test.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.P242;
+using Xunit;
+
+namespace LeetCode.UnitTests.P242;
+
+public class Test
+{
+    private readonly Solution _sut;
+
+    public Test()
+    {
+        _sut = new Solution();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Act
+        var result = _sut.IsAnagram("anagram", "nagaram");
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Act
+        var result = _sut.IsAnagram("rat", "car");
+
+        // Assert
+        Assert.False(result);
+    }
+}
---
+++ LeetCode/P242/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+
+namespace LeetCode.P242;
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        var characterOccurrence1 = GetCharacterOccurrence(s);
+        var characterOccurrence2 = GetCharacterOccurrence(t);
+
+        return characterOccurrence1.SequenceEqual(characterOccurrence2);
+    }
+
+    private static ImmutableSortedDictionary<char, int> GetCharacterOccurrence(string input)
+    {
+        return input
+            .GroupBy(c => c)
+            .ToImmutableSortedDictionary(grouping => grouping.Key, grouping => grouping.Count());
+    }
+}
