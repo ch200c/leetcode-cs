afa1ae4560ef26a5b6755622a608d1436dd7f472
ch200c <mantas.m@hotmail.com>
2023-09-16 10:35:49 +0100

    Solve P217, upgrade nuget packages

---
+++ LeetCode.UnitTests/P217/Test.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.P217;
+using Xunit;
+
+namespace LeetCode.UnitTests.P217;
+
+public class Test
+{
+    private readonly Solution _sut;
+
+    public Test()
+    {
+        _sut = new Solution();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Act
+        var result = _sut.ContainsDuplicate(new int[] { 1, 2, 3, 1 });
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Act
+        var result = _sut.ContainsDuplicate(new int[] { 1, 2, 3, 4 });
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        // Act
+        var result = _sut.ContainsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 });
+
+        // Assert
+        Assert.True(result);
+    }
+}
---
+++ LeetCode/P217/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+
+namespace LeetCode.P217;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var set = ImmutableHashSet.Create(nums);
+        return set.Count != nums.Length;
+    }
+}
