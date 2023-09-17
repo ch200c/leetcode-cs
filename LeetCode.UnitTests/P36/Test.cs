0f8bc8f54734047118bdc1cc21a35013cd125a68
ch200c <mantas.m@hotmail.com>
2023-09-17 22:34:48 +0100

    Solve P36

---
+++ LeetCode.UnitTests/P36/Test.cs
@@ -0,0 +1,59 @@
+﻿using Xunit;
+using LeetCode.P36;
+using System.Collections.Generic;
+
+namespace LeetCode.UnitTests.P36;
+
+public class Test
+{
+    public static IEnumerable<object[]> Test1Data =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                new char[][]
+                {
+                    new char[] { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                    new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                    new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                    new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                    new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                    new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                    new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                    new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                    new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+                },
+                true,
+            },
+            new object[]
+            {
+                new char[][]
+                {
+                    new char[] { '.', '.', '4', '.', '.', '.', '6', '3', '.' },
+                    new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                    new char[] { '5', '.', '.', '.', '.', '.', '.', '9', '.' },
+                    new char[] { '.', '.', '.', '5', '6', '.', '.', '.', '.' },
+                    new char[] { '4', '.', '3', '.', '.', '.', '.', '.', '1' },
+                    new char[] { '.', '.', '.', '7', '.', '.', '.', '.', '.' },
+                    new char[] { '.', '.', '.', '5', '.', '.', '.', '.', '.' },
+                    new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                    new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' }
+                },
+                false
+            }
+        };
+
+    [Theory]
+    [MemberData(nameof(Test1Data))]
+    public void Test1(char[][] board, bool expected)
+    {
+        // Arrange
+        var sut = new Solution();
+
+        // Act
+        var result = sut.IsValidSudoku(board);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode/P36/Solution.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.P36;
+
+public enum SubGrid
+{
+    TopLeft,
+    TopCenter,
+    TopRight,
+    MiddleLeft,
+    MiddleCenter,
+    MiddleRight,
+    BottomLeft,
+    BottomCenter,
+    BottomRight
+}
+
+public class Solution
+{
+    private readonly List<List<char>> _rows;
+    private readonly List<List<char>> _columns;
+    private readonly Dictionary<SubGrid, List<char>> _subGrids;
+
+    public Solution()
+    {
+        _rows = new List<List<char>>();
+        _columns = new List<List<char>>();
+
+        for (var i = 0; i < 9; i++)
+        {
+            _rows.Add(new List<char>());
+            _columns.Add(new List<char>());
+        }
+
+        _subGrids = new Dictionary<SubGrid, List<char>>()
+        {
+            { SubGrid.TopLeft, new List<char>() },
+            { SubGrid.TopCenter, new List<char>() },
+            { SubGrid.TopRight, new List<char>() },
+            { SubGrid.MiddleLeft, new List<char>() },
+            { SubGrid.MiddleCenter, new List<char>() },
+            { SubGrid.MiddleRight, new List<char>() },
+            { SubGrid.BottomLeft, new List<char>() },
+            { SubGrid.BottomCenter, new List<char>() },
+            { SubGrid.BottomRight, new List<char>() },
+        };
+    }
+
+    public bool IsValidSudoku(char[][] board)
+    {
+        for (var row = 0; row < 9; row++)
+        {
+            for (var column = 0; column < 9; column++)
+            {
+                if (board[row][column] != '.')
+                {
+                    _rows[row].Add(board[row][column]);
+                    _columns[column].Add(board[row][column]);
+
+                    var subGrid = GetSubGrid(row, column);
+                    _subGrids[subGrid].Add(board[row][column]);
+                }
+            }
+        }
+
+        for (var i = 0; i < 9; i++)
+        {
+            if (_rows[i].ToHashSet().Count != _rows[i].Count ||
+                _columns[i].ToHashSet().Count != _columns[i].Count)
+            {
+                return false;
+            }
+        }
+
+        return _subGrids[SubGrid.TopLeft].ToHashSet().Count == _subGrids[SubGrid.TopLeft].Count &&
+            _subGrids[SubGrid.TopCenter].ToHashSet().Count == _subGrids[SubGrid.TopCenter].Count &&
+            _subGrids[SubGrid.TopRight].ToHashSet().Count == _subGrids[SubGrid.TopRight].Count &&
+            _subGrids[SubGrid.MiddleLeft].ToHashSet().Count == _subGrids[SubGrid.MiddleLeft].Count &&
+            _subGrids[SubGrid.MiddleCenter].ToHashSet().Count == _subGrids[SubGrid.MiddleCenter].Count &&
+            _subGrids[SubGrid.MiddleRight].ToHashSet().Count == _subGrids[SubGrid.MiddleRight].Count &&
+            _subGrids[SubGrid.BottomLeft].ToHashSet().Count == _subGrids[SubGrid.BottomLeft].Count &&
+            _subGrids[SubGrid.BottomCenter].ToHashSet().Count == _subGrids[SubGrid.BottomCenter].Count &&
+            _subGrids[SubGrid.BottomRight].ToHashSet().Count == _subGrids[SubGrid.BottomRight].Count;
+    }
+
+    private static SubGrid GetSubGrid(int row, int column)
+    {
+        if (row <= 2 && column <= 2)
+        {
+            return SubGrid.TopLeft;
+        }
+        else if (row > 2 && row <= 5 && column <= 2)
+        {
+            return SubGrid.MiddleLeft;
+        }
+        else if (row > 5 && column <= 2)
+        {
+            return SubGrid.BottomLeft;
+        }
+        else if (row <= 2 && column > 2 && column <= 5)
+        {
+            return SubGrid.TopCenter;
+        }
+        else if (row > 2 && row <= 5 && column > 2 && column <= 5)
+        {
+            return SubGrid.MiddleCenter;
+        }
+        else if (row > 5 && column > 2 && column <= 5)
+        {
+            return SubGrid.BottomCenter;
+        }
+        else if (row <= 2 && column > 5)
+        {
+            return SubGrid.TopRight;
+        }
+        else if (row > 2 && row <= 5 && column > 5)
+        {
+            return SubGrid.MiddleRight;
+        }
+        else
+        {
+            return SubGrid.BottomRight;
+        }
+    }
+}
