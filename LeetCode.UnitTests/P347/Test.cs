f2792980ff720f2bb17fd203991de3fff1100047
ch200c <mantas.m@hotmail.com>
2023-09-17 18:15:06 +0100

    Solve P347

---
+++ LeetCode.UnitTests/P347/Test.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.P347;
+using System.Linq;
+using Xunit;
+
+namespace LeetCode.UnitTests.P347;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new int[] { 1, 1, 1, 2, 2, 3 }, 2, new int[] { 1, 2 })]
+    [InlineData(new int[] { 1 }, 1, new int[] { 1 })]
+    public void Test1(int[] nums, int k, int[] expected)
+    {
+        // Arrange
+        var sut = new Solution();
+
+        // Act
+        var result = sut.TopKFrequent(nums, k);
+
+        // Assert
+        Assert.Subset(expected.ToHashSet(), result.ToHashSet());
+    }
+}
---
+++ LeetCode/P347/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace LeetCode.P347;
+
+public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        return nums
+            .GroupBy(n => n)
+            .OrderByDescending(grouping => grouping.Count())
+            .Take(k)
+            .Select(grouping => grouping.Key)
+            .ToArray();
+    }
+}
