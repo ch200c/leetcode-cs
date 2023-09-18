59d00dc53ecf93851fc00c7eabba15c5c270a61d
ch200c <mantas.m@hotmail.com>
2023-09-18 07:58:48 +0100

    Solve P128

---
+++ LeetCode.UnitTests/P128/Test.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using LeetCode.P128;
+
+namespace LeetCode.UnitTests.P128;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new int[] { 100, 4, 200, 1, 3, 2 }, 4)]
+    [InlineData(new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }, 9)]
+    [InlineData(new int[] { }, 0)]
+    public void Test1(int[] nums, int expected)
+    {
+        // Arrange
+        var solution = new Solution();
+
+        // Act
+        var result = solution.LongestConsecutive(nums);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode/P128/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace LeetCode.P128;
+
+public class Solution
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        var longestConsecutiveCount = 0;
+        var consecutiveCount = 0;
+        int? previousNumber = null;
+
+        foreach (var number in nums.ToImmutableSortedSet())
+        {
+            if (previousNumber != null)
+            {
+                if (Math.Abs(number - previousNumber.Value) == 1)
+                {
+                    consecutiveCount++;
+                }
+                else
+                {
+                    longestConsecutiveCount = Math.Max(longestConsecutiveCount, consecutiveCount);
+                    consecutiveCount = 1;
+                }
+            }
+            else
+            {
+                consecutiveCount++;
+            }
+
+            previousNumber = number;
+        }
+
+        return Math.Max(longestConsecutiveCount, consecutiveCount);
+    }
+}
