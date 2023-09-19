62e09343bb05458ea289b19a81f54a74220f2788
ch200c <mantas.m@hotmail.com>
2023-09-19 22:18:12 +0100

    Solve P167

---
+++ LeetCode.UnitTests/P167/Test.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using LeetCode.P167;
+
+namespace LeetCode.UnitTests.P167;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 1, 2 })]
+    [InlineData(new int[] { 2, 3, 4 }, 6, new int[] { 1, 3 })]
+    [InlineData(new int[] { -1, 0 }, -1, new int[] { 1, 2 })]
+    public void Test1(int[] nums, int target, int[] expected)
+    {
+        // Arrange
+        var sut = new Solution();
+
+        // Act
+        var result = sut.TwoSum(nums, target);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode/P167/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.P167;
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var i = 0;
+        var j = numbers.Length - 1;
+
+        while (i < j)
+        {
+            var sum = numbers[i] + numbers[j];
+
+            if (sum == target)
+            {
+                return new int[] { i + 1, j + 1 };
+            }
+
+            if (sum > target)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+}
